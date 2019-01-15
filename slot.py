262246a9886aea1155510e918d0d7faf59a0a74d
Chandrika <chandrika.mjee@gmail.com>
2019-01-15 12:13:40 +0530

    Updated with slot

--- slot.py
+++ slot.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 import numpy as np
 import statistics
+import math
 
 
 
@@ -22,6 +23,11 @@ def time_parse(datestring):
 initial = '0'
 cflag=0
 node_contact={}
+total_list=[]
+nodes_vol={}
+nodes_time={}
+slot=0
+wr=0
 
 def takeSecond(elem):
 	return time_parse(elem[1])
@@ -29,47 +35,155 @@ def takeSecond(elem):
 
 
 def contact(file_name):
+	global wr
+	global slot
+	global total_vol
 	global cflag
 	global node_contact
 	global initial
+	global total_list
+	global nodes_time
+	global nodes_vol
 	logfile=open(file_name,'r')
 	logread = csv.reader(logfile)
 	if logread is not None:
 		for row in logread:
 			if cflag==0:
-				initial = row[0]
+				initial = row[1]
 				cflag =1
-				if row[1]==' PEER_DISCOVERED':
-					node_contact[row[2]]=1
+				node_contact[row[0]]=1
+				nodes_vol[row[0]]=int(row[4])
+				df = str(row[3]).split(':')
+				sm = 3600*int(df[0])+60*int(df[1])+int(df[2])
+				nodes_time[row[0]] = sm
+					
 			else:
-				current = row[0]
+				current = row[1]
 				diff = time_parse(current)-time_parse(initial)
 				ll = str(diff).split(',')
 				if(len(ll)==1):
 					df = str(diff).split(':')
 					sm = 3600*int(df[0])+60*int(df[1])+int(df[2])
 					if(sm>1800):
-						initial=row[0]
+						print(current,initial)
+						initial=row[1]
+						data_rate=0
+						cur =[]
+						dd=[]
+						slot +=1
 						for k,v in node_contact.items():
-							print(k)
-						print('**********************************************')
+							data_rate = nodes_vol[k]/nodes_time[k]
+							cur=[]
+							cur.append(slot)
+							cur.append(k)
+							cur.append(data_rate)
+							cur.append(nodes_time[k])
+							dd.append(cur)
+						print(dd)
+						with open('data_rate.csv','a') as wrfile:
+							writer = csv.writer(wrfile)
+							for u in range(len(dd)):
+								writer.writerow(dd[u])
+							wrfile.close()
+						xx=0
+						sm = sm -1800
+						print("sm = ",sm)
+						if sm%1800==0:
+							xx = (sm/1800)-1
+						else:
+							xx=math.floor(sm/1800)
+						print("xx = ",xx)
+						for u in range(xx):
+							cur=[]
+							slot +=1
+							cur.append(slot)
+							with open('data_rate.csv','a') as wrfile:
+								writer = csv.writer(wrfile)
+								writer.writerow(cur)
+								wrfile.close()
+							
 						node_contact={}
-						if(row[1]==' PEER_DISCOVERED'):
-							node_contact[row[2]]=1
+						nodes_vol={}
+						nodes_time={}
+						node_contact[row[0]]=1
+						nodes_vol[row[0]]=int(row[4])
+						df = str(row[3]).split(':')
+						sm = 3600*int(df[0])+60*int(df[1])+int(df[2])
+						nodes_time[row[0]] = sm
+						wr=1
 					else:
-						if(row[1]==' PEER_DISCOVERED'):
-							node_contact[row[2]]=1
+						wr=0
+						node_contact[row[0]]=1
+						if row[0] in nodes_vol:
+							nodes_vol[row[0]] += int(row[4])
+							
+						else:
+							nodes_vol[row[0]]=int(row[4])
+							
+						if row[0] in nodes_time:
+							df = str(row[3]).split(':')
+							sm = 3600*int(df[0])+60*int(df[1])+int(df[2])
+							nodes_time[row[0]] += sm
+							
+						else:
+							df = str(row[3]).split(':')
+							sm = 3600*int(df[0])+60*int(df[1])+int(df[2])
+							nodes_time[row[0]] = sm
 				else:
-					initial = row[0]
+					print("I am getting more than one day gap")
+					initial=row[1]
+					data_rate=0
+					cur =[]
+					dd=[]
+					slot +=1
 					for k,v in node_contact.items():
-						print(k)
-					print('**********************************************')
+						data_rate = nodes_vol[k]/nodes_time[k]
+						cur=[]
+						cur.append(slot)
+						cur.append(k)
+						cur.append(data_rate)
+						cur.append(nodes_time[k])
+						dd.append(cur)
+					print(dd)
+					with open('data_rate.csv','a') as wrfile:
+						writer = csv.writer(wrfile)
+						for u in range(len(dd)):
+							writer.writerow(dd[u])
+						wrfile.close()
 					node_contact={}
-					if(row[1]==' PEER_DISCOVERED'):
-						node_contact[row[2]]=1
-					else:
-						if(row[1]==' PEER_DISCOVERED'):
-							node_contact[row[2]]=1
+					nodes_vol={}
+					nodes_time={}
+					node_contact[row[0]]=1
+					nodes_vol[row[0]]=int(row[4])
+					df = str(row[3]).split(':')
+					sm = 3600*int(df[0])+60*int(df[1])+int(df[2])
+					nodes_time[row[0]] = sm
+					wr=1
+					
+
+
+	if wr==0:
+		print("I am getting here ")
+		cur =[]
+		dd=[]
+		slot +=1
+		for k,v in node_contact.items():
+			data_rate = nodes_vol[k]/nodes_time[k]
+			cur=[]
+			cur.append(slot)
+			cur.append(k)
+			cur.append(data_rate)
+			cur.append(nodes_time[k])
+			dd.append(cur)
+		print(dd)
+		with open('data_rate.csv','a') as wrfile:
+			writer = csv.writer(wrfile)
+			for u in range(len(dd)):
+				writer.writerow(dd[u])
+			wrfile.close()
+
+							
+						
 
 
 
@@ -90,11 +204,12 @@ for cur_file in f_list:
 		ll +=1
 	if ll==0:
 		name=os.path.basename(n_file.name)
-		fg= name.split('_')
-		ns=name.split('-')
-		if(ns[0]=="psyncLog"):
+		if(name.endswith('connection_time.csv')):
 			print(cur_file)
 			contact(cur_file)
-			ff_name = name
+
+for j in range(len(total_list)):
+	print(total_list[j])
+	print('*************************************************')
 			
 
